9368b81ea3a7a046e3fd1a41de53e823a5849dd2
Daniel Raymond <dan@raymond.ch>
2021-08-26 10:48:08 -0400

    Found proper Q equation!

--- Q_extraction.py
+++ Q_extraction.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 # Returns IQ data
 # ! w0 setting is incorrect
 def createData(Q, w0, F0=1, num_points=1000, noise=0.01, plot=False):
-	freqs = np.linspace(w0 / 2, w0*2 + 10, num_points)
+	freqs = np.linspace(w0-w0/Q, w0+w0/Q, num_points)
 	lorentzFunc = lambda f: F0 / np.sqrt((1-(f**2)/(w0**2))**2 + (f/(Q*w0))**2)
 	lorentz = lorentzFunc(freqs) + noise*np.random.randn(len(freqs))
 
@@ -38,13 +38,14 @@ def fitAmpVsPhase(iq, plot=False):
 def getQ(coef: tuple):
 	print(coef)
 	assert len(coef) == 3, "Polyfit must be degree=3"
-	# ! Coefficients must be in root form, which they are not
 	c, b, a = coef
-	Q = c / (2 * b)
-	return Q
+	p_c = np.pi/2
+	# Q = (c + b*p_c + a*(p_c**2)) / (2*b + 4*a*p_c)
+	Q = (4*a*c - b**2) / (8*a*(b + a*np.pi))
+	return np.abs(Q)
 
 if __name__ == '__main__':
-	Q = 10
-	iq = createData(Q=Q, w0=5, F0=1/Q, plot=True)
-	coef = fitAmpVsPhase(iq)
+	Q = 50000
+	iq = createData(Q=Q, w0=5000, F0=1/Q, noise=0, plot=True)
+	coef = fitAmpVsPhase(iq, plot=True)
 	print(getQ(coef))
