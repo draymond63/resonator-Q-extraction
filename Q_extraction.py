f79cdd30774c67f364834a5fdb342b879feeba73
Daniel Raymond <dan@raymond.ch>
2021-08-25 22:24:27 -0400

    Fixed polyfit more, found root cause of off Q

--- Q_extraction.py
+++ Q_extraction.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Returns IQ data
+# ! w0 setting is incorrect
 def createData(Q, w0, F0=1, num_points=1000, noise=0.01, plot=False):
 	freqs = np.linspace(w0 / 2, w0*2 + 10, num_points)
 	lorentzFunc = lambda f: F0 / np.sqrt((1-(f**2)/(w0**2))**2 + (f/(Q*w0))**2)
@@ -13,7 +14,7 @@ def createData(Q, w0, F0=1, num_points=1000, noise=0.01, plot=False):
 	iq = lorentz * np.exp(arc * 1j)
 
 	if plot:
-		fig, axs = plt.subplots(2, 1)
+		_, axs = plt.subplots(2, 1)
 		axs[0].plot(lorentz)
 		axs[1].plot(arc)
 		plt.show()
@@ -22,25 +23,28 @@ def createData(Q, w0, F0=1, num_points=1000, noise=0.01, plot=False):
 # Returns polynomial
 def fitAmpVsPhase(iq, plot=False):
 	amp = np.abs(iq)
-	phase = np.unwrap(np.angle(iq))
+	phase = np.angle(iq)
+	coef = np.polynomial.polynomial.polyfit(phase, amp, deg=2, w=amp)
 
-	poly = np.polynomial.polynomial.Polynomial.fit(phase, amp, deg=2).convert().coef
-	print(poly)
 	if plot:
-		fit = np.polynomial.polynomial.polyval(phase, poly)
+		fit_x = np.linspace(0, np.pi, len(phase))
+		fit_y = np.polynomial.polynomial.polyval(fit_x, coef)
 		plt.plot(phase, amp, 'o')
-		plt.plot(phase, fit)
+		plt.plot(fit_x, fit_y)
 		plt.show()
-	return poly
+	return coef
 
 # Gets Q
 def getQ(coef: tuple):
+	print(coef)
+	assert len(coef) == 3, "Polyfit must be degree=3"
+	# ! Coefficients must be in root form, which they are not
 	c, b, a = coef
-	Q = b / (2 * c)
+	Q = c / (2 * b)
 	return Q
 
 if __name__ == '__main__':
-	Q = 1000
-	iq = createData(Q=Q, w0=5, F0=1/Q, noise=0, num_points=10000, plot=True)
+	Q = 10
+	iq = createData(Q=Q, w0=5, F0=1/Q, plot=True)
 	coef = fitAmpVsPhase(iq)
 	print(getQ(coef))
