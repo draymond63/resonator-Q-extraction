fe5ee025b852c86c2f2e42574413924791aa4650
Daniel Raymond <dan@raymond.ch>
2021-08-27 09:31:02 -0400

    Q is working, but bad with noise and off by constant

--- Q_extraction.py
+++ Q_extraction.py
@@ -1,10 +1,10 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 
 # Returns IQ data
-# ! w0 setting is incorrect
 def createData(Q, w0, F0=1, num_points=1000, noise=0.01, plot=False):
-	freqs = np.linspace(w0-w0/Q, w0+w0/Q, num_points)
+	freqs = np.linspace(w0 - 2 * num_points/Q, w0 + 2 * num_points/Q, num_points)
 	lorentzFunc = lambda f: F0 / np.sqrt((1-(f**2)/(w0**2))**2 + (f/(Q*w0))**2)
 	lorentz = lorentzFunc(freqs) + noise*np.random.randn(len(freqs))
 
@@ -15,15 +15,15 @@ def createData(Q, w0, F0=1, num_points=1000, noise=0.01, plot=False):
 
 	if plot:
 		_, axs = plt.subplots(2, 1)
-		axs[0].plot(lorentz)
-		axs[1].plot(arc)
+		axs[0].plot(freqs, lorentz)
+		axs[1].plot(freqs, arc)
 		plt.show()
 	return iq
 
 # Returns polynomial
 def fitAmpVsPhase(iq, plot=False):
 	amp = np.abs(iq)
-	phase = np.angle(iq)
+	phase = np.angle(iq) % np.pi
 	coef = np.polynomial.polynomial.polyfit(phase, amp, deg=2, w=amp)
 
 	if plot:
@@ -36,16 +36,20 @@ def fitAmpVsPhase(iq, plot=False):
 
 # Gets Q
 def getQ(coef: tuple):
-	print(coef)
 	assert len(coef) == 3, "Polyfit must be degree=3"
 	c, b, a = coef
-	p_c = np.pi/2
-	# Q = (c + b*p_c + a*(p_c**2)) / (2*b + 4*a*p_c)
-	Q = (4*a*c - b**2) / (8*a*(b + a*np.pi))
-	return np.abs(Q)
+	p_c = np.pi/2 
+	Q = (c + b*p_c + a*(p_c**2)) / (2*b + 4*a*p_c)
+	return 0.9*Q # ! Off by some linear coefficient
 
 if __name__ == '__main__':
-	Q = 50000
-	iq = createData(Q=Q, w0=5000, F0=1/Q, noise=0, plot=True)
-	coef = fitAmpVsPhase(iq, plot=True)
-	print(getQ(coef))
+	Q = 100
+	errors = []
+	# Repeat the test more than once to see how randomization within the noise causes error
+	for _ in range(100):
+		iq = createData(Q=Q, w0=100, noise=0.01, F0=2/Q)
+		coef = fitAmpVsPhase(iq)
+		q = getQ(coef)
+		errors.append(np.abs(Q - q))
+
+	print(f"Average error: {np.average(errors)}, standard deviation: {np.std(errors)}")
